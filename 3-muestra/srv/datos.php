007fb5dc360bfd6b5325c78093a695c813d7cb7b
MaximoCrz056 <maximogomezcruz@gmail.com>
2024-11-05 10:37:25 -0600

    muestra funcional

--- 3-muestra/srv/datos.php
+++ 3-muestra/srv/datos.php
@@ -2,6 +2,19 @@
 
 require_once __DIR__ . "/../lib/php/devuelveJson.php";
 
+$jsonData = file_get_contents("https://jsonplaceholder.typicode.com/users/1");
+
+if ($jsonData === false) {
+    die("Error al obtener los datos.");
+}
+
+$jsonData = json_decode($jsonData);
+
 devuelveJson([
-    "id"=> "$id",
+    "id" => ["value" => $jsonData->id],
+    "name" => ["value" => $jsonData->name],
+    "username" => ["value" => $jsonData->username],
+    "email" => ["value"=> $jsonData->email],
+    "phone" => ["value"=> $jsonData->phone],
+    "website" => ["value"=> $jsonData->website],
 ]);
